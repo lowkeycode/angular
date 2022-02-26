9c78fe5c54ee3820918f25ca513a21ec730c8c49
Cam Remesz <saltyndead@gmail.com>
2022-02-26 10:48:16 -0700

    Practice event binding passing up multiple levels

--- recipe-shopper/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopper/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -9,12 +9,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test', 'This is only a test', 'https://picsum.photos/200')
+    new Recipe('Test', 'This is only a test', 'https://picsum.photos/200'),
+    new Recipe('Testy McTestFace', 'Testing', 'https://picsum.photos/200'),
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
